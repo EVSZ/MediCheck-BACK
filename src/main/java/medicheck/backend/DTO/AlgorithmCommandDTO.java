2b8fd5de4f979dafc38386d3eb90a1ad7a71caf0
Dylan <449904@student.fontys.nl>
2021-04-30 10:47:30 +0200

    SubRule ophalen changes
    Subrule ophalen

--- src/main/java/medicheck/backend/DTO/AlgorithmCommandDTO.java
+++ src/main/java/medicheck/backend/DTO/AlgorithmCommandDTO.java
@@ -1,8 +1,11 @@
 package medicheck.backend.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import medicheck.backend.Algoritmiek.AlgorithmCommand;
 import medicheck.backend.Algoritmiek.Entities.AlgorithmCommandEntity;
 
+@Getter @Setter
 public class AlgorithmCommandDTO
 {
     long ID;
